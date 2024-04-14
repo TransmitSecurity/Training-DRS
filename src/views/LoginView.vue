3cd1d2ce87f598160c38a954e4ecac3bbd224c3c
Maurice Luizink <111053408+mluizinkTS@users.noreply.github.com>
2024-04-14 20:09:45 +0200

    Update LoginView.vue
    Fixed error in view. Move of code.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -141,9 +141,8 @@ async function loginWebauthn() {
 
 async function loginWebauthnAutofill(webauthnEncodedResult: string) {
   try {
-    // WEBINAR ACTION - Report the login action
-    const respon     reportAction(Action.LOGIN);
-    se = await authApi.authenticateWebauthn({
+    reportAction(Action.LOGIN);
+    const response = await authApi.authenticateWebauthn({
       webauthnEncodedResult,
     });
     console.log(response);
